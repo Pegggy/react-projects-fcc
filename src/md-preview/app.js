49c72e390260930c48fadf29cbe504c4d9b24100
penfan <fangpei9212@163.com>
2017-11-17 16:42:04 +0800

    引入 marked，使用dangerouslySetInnerHTML属性实时显示转化后的 md

--- src/md-preview/app.js
+++ src/md-preview/app.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react'
+let marked = require('marked')
 
 class Input extends Component{
   constructor(props){
@@ -12,10 +13,18 @@ class Input extends Component{
       value: e.target.value
     })
   }
+  getMarked(value){
+    return {__html: marked(this.state.value)}
+  }
   render(){
     return(
       <div>
-        <textarea value={this.state.value} onChange={this.handleChange.bind(this)}/>
+        <div className="left">
+          <textarea value={this.state.value} onChange={this.handleChange.bind(this)}/>
+        </div>
+        <div className="right" dangerouslySetInnerHTML={this.getMarked(this.state.value)}>
+          
+        </div>
       </div>
     )
   }
